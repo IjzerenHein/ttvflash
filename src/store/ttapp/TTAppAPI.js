42b11ffe9d5a9a3276ee43e39facaf6c0ed23a3a
Hein Rutjes <hrutjes@gmail.com>
2019-09-27 11:17:02 +0200

    disabled dev-cache & debug logging

--- src/store/ttapp/TTAppAPI.js
+++ src/store/ttapp/TTAppAPI.js
@@ -1,7 +1,7 @@
 /* @flow */
 import type { Poule, Match, Club, Group, Semester } from './types';
 
-const ENABLE_CACHE = true;
+const ENABLE_CACHE = false;
 
 export class TTAppAPI {
   _token = undefined;
@@ -43,7 +43,7 @@ export class TTAppAPI {
     }
 
     const body = JSON.stringify(fields);
-    console.log(`FETCHing ${body}...`);
+    //console.log(`FETCHing ${body}...`);
     try {
       const response = await fetch('https://ttapp.nl/api', {
         method: 'post',
@@ -51,10 +51,10 @@ export class TTAppAPI {
       });
       if (response.status !== 200)
         throw new Error(`Invalid status: ${response.status}`);
-      console.log(`FETCHing DONE (status: ${response.status})`);
+      //console.log(`FETCHing DONE (status: ${response.status})`);
       const json = await response.json();
       localStorage.setItem(cacheKey, JSON.stringify(json));
-      console.log(json);
+      //console.log(json);
       return json;
     } catch (err) {
       console.log(`FETCH FAILED (${err.message})`);
